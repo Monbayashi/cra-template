79dba47058a7596f5e538d3ff9b6dd541a8c4b54
monbayashi <monbayashi@saterise.com.jp>
2022-10-31 11:39:56 +0900

    デザイン少し修正

--- src/components/layouts/sidebars/SideBarTitle.tsx
+++ src/components/layouts/sidebars/SideBarTitle.tsx
@@ -5,7 +5,7 @@ type Props = {
 export const SideBarTitle: React.FC<Props> = ({ title }) => (
   <li className='hidden px-5 md:block'>
     <div className='flex h-8 flex-row items-center'>
-      <div className='text-sm font-light tracking-wide text-gray-400 uppercase'>{title}</div>
+      <div className='text-sm font-light tracking-wide text-gray-300 uppercase'>{title}</div>
     </div>
   </li>
 );
