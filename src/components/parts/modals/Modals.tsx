76d193b2c8aba3e4c2ea0cd8fad5f1e6fe55f2f9
monbayashi <monbayashi@saterise.com.jp>
2022-10-31 10:46:49 +0900

    modal animation 追加

--- src/components/parts/modals/Modals.tsx
+++ src/components/parts/modals/Modals.tsx
@@ -9,21 +9,36 @@ type Props = {
 };
 
 const classNames = {
-  modal: lintName(
-    'min-w-[320px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute bg-gray-200 z-50 rounded-lg shadow',
-  ),
-  overlay: lintName('z-40 bg-[#00000096] inset-0 fixed cursor-default'),
-};
+  modal: {
+    base: lintName(
+      'min-w-[320px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute bg-gray-200 z-50 rounded-lg shadow',
+    ),
+  },
+  overlay: {
+    base: lintName(
+      'z-40 bg-[#00000096] inset-0 fixed cursor-default transition-all duration-300 opacity-0',
+    ),
+    afterOpen: lintName('!opacity-100'),
+  },
+} as const;
 
 export const SimpleModal: React.FC<Props> = ({ children, isOpen, closeHandling }) => {
   return (
     <Modal
       isOpen={isOpen}
-      closeTimeoutMS={100}
+      closeTimeoutMS={300}
       shouldCloseOnOverlayClick={true}
       onRequestClose={closeHandling}
-      className={classNames.modal}
-      overlayClassName={classNames.overlay}
+      className={{
+        base: classNames.modal.base,
+        afterOpen: '',
+        beforeClose: '',
+      }}
+      overlayClassName={{
+        base: classNames.overlay.base,
+        afterOpen: isOpen ? classNames.overlay.afterOpen : '',
+        beforeClose: '',
+      }}
       appElement={document.getElementById('root') || undefined}
     >
       {children}
